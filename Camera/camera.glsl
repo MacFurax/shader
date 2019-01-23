c8de067221763c355a3b516125d5177e0761713f
Constant Dupuis <dev-tools@alterface.com>
2019-01-23 14:44:42 +0100

    Start Camera tutorial

--- Camera/camera.glsl
+++ Camera/camera.glsl
@@ -1,7 +1,32 @@
+//
+// https://www.youtube.com/watch?v=PBxuVlp7nuM
+// 
+
+//
+// This function return the distance from a point in 3D space 
+// to the view ray
+//
 float DistLine( vec3 ro, vec3 rd, vec3 p)
 {
     return length(cross(p-ro, rd))/length(rd);
 }
+
+// draw a point
+// ro: ray origine
+// rd: ray direction
+// p: point 
+//
+float DrawPoint(vec3 ro, vec3 rd, vec3 p)
+{
+// get the distance from the point p to the view ray
+    float d = DistLine( ro, rd, p);
+
+    // make it a dot not a degrade
+    // comment this line to see distance value
+    d = smoothstep(.06,.05, d);
+    return d;
+}
+
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
@@ -9,14 +34,23 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     uv -= .5; // -.5 <> .5
     uv.x *= iResolution.x/iResolution.y; // compensate for aspect ratio
 
+    // warning if we move the camera back allow we zomm in
+    // field of view become narrower
+    // so we nned to also move the screen back
     vec3 ro = vec3(0.,0.,-3.); //ray origine (eye) in front of screen
-    vec3 rd = vec3(uv.x, uv.y, 0.) - ro;
+    vec3 rd = vec3(uv.x, uv.y, -2.) - ro; // ray vector from origine to point on screen
+        
+    float d = 0.;
 
-    float t =  iGlobalTime;
-    vec3 p = vec3(sin(t),0.,1.+cos(t)*2.);
-    float d = DistLine( ro, rd, p);
+    d += DrawPoint( ro, rd, vec3(0.,0.,0.));
+    d += DrawPoint( ro, rd, vec3(0.,0.,1.));
+    d += DrawPoint( ro, rd, vec3(0.,1.,0.));
+    d += DrawPoint( ro, rd, vec3(0.,1.,1.));
 
-    d = smoothstep(.1,.09, d);
+    d += DrawPoint( ro, rd, vec3(1.,0.,0.));
+    d += DrawPoint( ro, rd, vec3(1.,0.,1.));
+    d += DrawPoint( ro, rd, vec3(1.,1.,0.));
+    d += DrawPoint( ro, rd, vec3(1.,1.,1.));
 
     fragColor = vec4(d);
 }
--- TheSimplest3D/theSimplest3D.glsl
+++ TheSimplest3D/theSimplest3D.glsl
@@ -1,3 +1,11 @@
+//
+// Based on https://www.youtube.com/watch?v=dKA5ZVALOhs
+// 
+
+//
+// This function return the distance from a point in 3D space 
+// to the view ray
+//
 float DistLine( vec3 ro, vec3 rd, vec3 p)
 {
     return length(cross(p-ro, rd))/length(rd);
@@ -10,12 +18,17 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     uv.x *= iResolution.x/iResolution.y; // compensate for aspect ratio
 
     vec3 ro = vec3(0.,0.,-3.); //ray origine (eye) in front of screen
-    vec3 rd = vec3(uv.x, uv.y, 0.) - ro;
+    vec3 rd = vec3(uv.x, uv.y, 0.) - ro; // ray vector from origine to point on screen
 
+    // move the point in 3D space
     float t =  iGlobalTime;
     vec3 p = vec3(sin(t),0.,1.+cos(t)*2.);
+    
+    // get the distance from the point p to the view ray
     float d = DistLine( ro, rd, p);
 
+    // make it a dot not a degrade
+    // comment this line to see distance value
     d = smoothstep(.1,.09, d);
 
     fragColor = vec4(d);
