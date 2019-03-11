4d97d270d3f9279728e11ed0f2ab9c9d88728f15
Unknown <constant.dupuis@gmail.com>
2019-03-11 15:31:59 +0100

    changes stuff

--- TheBookOfShaders/Colors/colors 05.glsl
+++ TheBookOfShaders/Colors/colors 05.glsl
@@ -27,8 +27,7 @@ vec3 rgb2hsb( in vec3 c ){
 //  Function from Iñigo Quiles
 //  https://www.shadertoy.com/view/MsS3Wc
 vec3 hsb2rgb( in vec3 c ){
-    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
-                             6.0)-3.0)-1.0,
+    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0), 6.0)-3.0)-1.0,
                      0.0,
                      1.0 );
     rgb = rgb*rgb*(3.0-2.0*rgb);
